3b6a6c814fe6d0986cb0740bc73dec7429b8c31f
Tom Saunders <47544177+TomSaunders99@users.noreply.github.com>
2019-02-26 14:27:41 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>Test</title>
+</head>
+
+<body>
+    
+    <?php echo "<h1>hello world</h1>" ?>
+    
+    <list>
+        <ul>
+            <li>one</li>
+            <li>two</li>
+        </ul>
+    </list>
+</body>
+    
+</html>
